e181df7037c38ad2efa33f9a8abb3b325b927095
ALSijsling <125027260+ALSijsling@users.noreply.github.com>
2024-03-26 15:23:10 +0100

    added vue and vue-router

---
+++ resources/js/router/routes.ts
@@ -0,0 +1,7 @@
+import Home from './../domains/books/pages/Overview.vue';
+
+const routes = [
+    {path: '/', name: 'Home', component: Home}
+];
+
+export default routes;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/js/**/*'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
