0798442c732d1ad75844c48e8f7a5b52eb2fd6e9
ALSijsling <125027260+ALSijsling@users.noreply.github.com>
2024-06-04 15:39:38 +0200

    add link to authors overview page

--- resources/js/router/routes.ts
+++ resources/js/router/routes.ts
@@ -1,13 +1,15 @@
-import Home from './../domains/books/pages/Overview.vue';
-import CreateBook from './../domains/books/pages/Create.vue';
-import ShowBook from './../domains/books/pages/Show.vue';
-import EditBook from './../domains/books/pages/Edit.vue';
+import Home from '../domains/books/pages/Overview.vue';
+import CreateBook from '../domains/books/pages/Create.vue';
+import ShowBook from '../domains/books/pages/Show.vue';
+import EditBook from '../domains/books/pages/Edit.vue';
+import Authors from '../domains/authors/pages/Overview.vue';
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
     {path: '/create', name: 'CreateBook', component: CreateBook},
     {path: '/books/:id', name: 'ShowBook', component: ShowBook},
-    {path: '/books/:id/edit', name: 'EditBook', component: EditBook}
+    {path: '/books/:id/edit', name: 'EditBook', component: EditBook},
+    {path: '/authors', name: 'Authors', component: Authors}
 ];
 
 export default routes;
