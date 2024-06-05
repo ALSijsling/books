a0ed0e16e94ef0ce3b887f08cf3aa2ffa4d0dd5f
ALSijsling <125027260+ALSijsling@users.noreply.github.com>
2024-06-05 12:28:42 +0200

    add create author link to navigation

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-    <nav class="bg-gray-800 text-slate-200 text-xl py-3">
-        <router-link :to="{name: 'Home'}" class="p-3 hover:bg-gray-500">Home</router-link>
-        <router-link :to="{name: 'Authors'}" class="p-3 hover:bg-gray-500">Authors</router-link>
-        <router-link :to="{name: 'CreateBook'}" class="p-3 hover:bg-gray-500">Add Book</router-link>
+    <nav class="flex justify-between bg-gray-800 text-slate-200 text-xl py-3">
+        <div>
+            <router-link :to="{name: 'Home'}" class="p-3 hover:bg-gray-500">Home</router-link>
+            <router-link :to="{name: 'Authors'}" class="p-3 hover:bg-gray-500">Authors</router-link>
+        </div>
+        <div>
+            <router-link :to="{name: 'CreateBook'}" class="p-3 hover:bg-gray-500">Add Book</router-link>
+            <router-link :to="{name: 'CreateAuthor'}" class="p-3 hover:bg-gray-500">Add Author</router-link>
+        </div>
     </nav>
 </template>
--- resources/js/router/routes.ts
+++ resources/js/router/routes.ts
@@ -3,13 +3,15 @@ import CreateBook from '../domains/books/pages/Create.vue';
 import ShowBook from '../domains/books/pages/Show.vue';
 import EditBook from '../domains/books/pages/Edit.vue';
 import Authors from '../domains/authors/pages/Overview.vue';
+import CreateAuthor from '../domains/authors/pages/Create.vue';
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
     {path: '/books/create', name: 'CreateBook', component: CreateBook},
     {path: '/books/:id', name: 'ShowBook', component: ShowBook},
     {path: '/books/:id/edit', name: 'EditBook', component: EditBook},
-    {path: '/authors', name: 'Authors', component: Authors}
+    {path: '/authors', name: 'Authors', component: Authors},
+    {path: '/authors/create', name: 'CreateAuthor', component: CreateAuthor}
 ];
 
 export default routes;
